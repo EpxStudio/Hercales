a2a756b20d4e41c63d75cae9928f0eb978920b41
Livio De La Cruz <livio@livio.me>
2018-03-16 09:40:58 -0500

    guy animates better now

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
 	public Animator anim;
+	public Rigidbody2D rb;
+
+	private bool isStanding = false;
 
 	private void Update()
 	{
+		anim.SetBool("isStanding", isStanding);
+		anim.SetFloat("ySpeed", rb.velocity.y);
 		if (Input.GetMouseButtonDown(0))
 		{
 			// TODO: position of object at middle (maybe use center of mass?)
-			Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
 			this.Jump(rb, Camera.main.ScreenToWorldPoint(Input.mousePosition));
 		}
+		if (rb.velocity.x > 0)
+			anim.transform.localScale = new Vector3(1, 1, 1);
+		if (rb.velocity.x < 0)
+			anim.transform.localScale = new Vector3(-1, 1, 1);
 	}
 
 	private void Jump(Rigidbody2D jumper, Vector2 apexPosition)
