6d3adddcb5041870fa928a1ee342a2944e5333d1
Livio De La Cruz <livio@livio.me>
2018-03-17 01:55:05 -0500

    added lion sprite

--- Assets/Scripts/Grabbable.cs
+++ Assets/Scripts/Grabbable.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class Grabbable : MonoBehaviour
 {
     private Rigidbody2D rb;
-	// Use this for initialization
 	void Start () {
         rb = this.GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(1)) {
 			Arc.Apex(rb, Camera.main.ScreenToWorldPoint(Input.mousePosition));
